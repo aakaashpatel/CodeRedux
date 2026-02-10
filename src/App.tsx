43540ca18e85a80c8affcc7662c91e6bd309fa92
Aakash Patel <akashpatel8818@gmail.com>
2026-02-10 22:27:43 +0530

    Adding the dispatch and selector

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
+import { decrement, increment } from "./redux/features/counterSlice";
 const App = () => {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
   return (
     <div className="main">
-      <p className="resultValue">0</p>
+      <p className="resultValue">{count}</p>
       <div className="mainBtn">
-        <button className="btn" handleIncrement = {handleIncrement()}>Increment</button>
-        <button className="btn">Decrement</button>
+        <button className="btn" onClick = {() => {
+          dispatch(increment());
+        }}>Increment</button>
+        <button className="btn" onClick = {() =>{
+          dispatch(decrement());
+        }}>Decrement</button>
       </div>
     </div>
   );
